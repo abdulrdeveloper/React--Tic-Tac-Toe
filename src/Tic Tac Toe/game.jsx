5e293d9b4a1bca3b029f616026b2c42b4b653d04
abdulrdeveloper <abdulrahman2040b@gmail.com>
2026-05-16 13:23:47 +0500

    Implement game logic and UI for Tic Tac Toe; add win/draw conditions and toggle functionality

--- src/Tic Tac Toe/game.jsx
+++ src/Tic Tac Toe/game.jsx
@@ -1,9 +1,99 @@
-import React from 'react'
+import React, { use } from "react";
 
-const game = () => {
-  return (
-    <div>game</div>
-  )
+let data = [
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""],
+];
+
+let [count, setCount] = useState(0);
+let [lock, setLock] = useState(false);
+
+
+const toggle = (w,num) => {
+    if (lock){
+        return;
+    }
+    if(count % 2 === 0){
+        e.target.innerText = `<img src='${cross_icon}' alt="X" />`;
+        data[num] = "X";
+        setCount(count + 1);
+    }
+    else {
+        e.target.innerText = `<img src='${circle_icon}' alt="O" />`;
+        data[num] = "O";
+        setCount(count + 1);
+    }
+    checkWin();
+};
+
+const checkWin = () => {
+    if(data[0]===data[1] && data[1] === data[2] && data[2] !== ""){
+        won(data);
+    }
+    else if (data[3]===data[4] && data[4] === data[5] && data[5] !== ""){
+        won(data);
+    }
+    else if (data[6]===data[7] && data[7] === data[8] && data[8] !== ""){
+        won(data);
+    }
+    else if (data[0]===data[3] && data[3] === data[6] && data[6] !== ""){
+        won(data);
+    }
+    else if (data[1]===data[4] && data[4] === data[7] && data[7] !== ""){
+        won(data);
+    }
+    else if (data[2]===data[5] && data[5] === data[8] && data[8] !== ""){
+        won(data);
+    }
+    else if (data[0]===data[4] && data[4] === data[8] && data[8] !== ""){
+        won(data);
+    }
+    else if (data[0]===data[1] && data[1] === data[2] && data[2] !== ""){
+        won(data);
+    }
+    else if (data[2]===data[4] && data[4] === data[6] && data[6] !== ""){
+        won(data);
+    }
+    else if (count === 9){
+        setLock(true);
+        alert("It's a draw!");
+    }
 }
 
-export default game
+const won = (winner) => {
+    setLock(true);
+    alert(`${winner} wins!`);
+}
+
+const Game = () => {
+    return (
+        <>
+            <div>
+                <h1>Tic Tac Toe in React</h1>
+                <button>Reset Game</button>
+            </div>
+            <div className="container">
+                <div className="game-board">
+                    <div className="row" onClick={(e) => toggle(e, 0)}></div>
+                    <div className="row" onClick={(e) => toggle(e, 1)}></div>
+                    <div className="row" onClick={(e) => toggle(e, 2)}></div>
+                </div>
+
+                <div className="game-board">
+                    <div className="row" onClick={(e) => toggle(e, 3)}></div>
+                    <div className="row" onClick={(e) => toggle(e, 4)}></div>
+                    <div className="row" onClick={(e) => toggle(e, 5)}></div>
+                </div>
+
+                <div className="game-board">
+                    <div className="row" onClick={(e) => toggle(e, 6)}></div>
+                    <div className="row" onClick={(e) => toggle(e, 7)}></div>
+                    <div className="row" onClick={(e) => toggle(e, 8)}></div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Game;
